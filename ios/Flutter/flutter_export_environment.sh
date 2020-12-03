ddf928062808c0f7a25a236d5935072e63dc99df
Jewel Rana <tonujewel@gmail.com>
2020-12-03 17:22:02 +0600

    no changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/jewel/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/mnt/WorkSpace/Flutter/Download/flutter_notes"
+export "FLUTTER_APPLICATION_PATH=/mnt/WorkSpace/Flutter/Download/flutter-keep"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,87 @@
 import 'package:flutter/material.dart';
-import 'ViewControllers/HomePage.dart';
-import 'package:flutter/rendering.dart';
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'models.dart' show CurrentUser;
+import 'screens.dart' show HomeScreen, LoginScreen, NoteEditor, SettingsScreen;
+import 'styles.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(NotesApp());
+
+class NotesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: "Roboto",
-        iconTheme: IconThemeData(color: Colors.black),
-        primaryTextTheme: TextTheme(
-          title: TextStyle(color: Colors.black),
+  Widget build(BuildContext context) => StreamProvider.value(
+        value: FirebaseAuth.instance.onAuthStateChanged
+            .map((user) => CurrentUser.create(user)),
+        initialData: CurrentUser.initial,
+        child: Consumer<CurrentUser>(
+          builder: (context, user, _) => MaterialApp(
+            title: 'Flutter Keep',
+            theme: Theme.of(context).copyWith(
+              brightness: Brightness.light,
+              primaryColor: Colors.white,
+              accentColor: kAccentColorLight,
+              appBarTheme: AppBarTheme.of(context).copyWith(
+                elevation: 0,
+                brightness: Brightness.light,
+                iconTheme: IconThemeData(
+                  color: kIconTintLight,
+                ),
+              ),
+              scaffoldBackgroundColor: Colors.white,
+              bottomAppBarColor: kBottomAppBarColorLight,
+              primaryTextTheme: Theme.of(context).primaryTextTheme.copyWith(
+                    // title
+                    headline6: const TextStyle(
+                      color: kIconTintLight,
+                    ),
+                  ),
+            ),
+            home: user.isInitialValue
+                ? Scaffold(body: const SizedBox())
+                : user.data != null
+                    ? HomeScreen()
+                    : LoginScreen(),
+            routes: {
+              '/settings': (_) => SettingsScreen(),
+            },
+            onGenerateRoute: _generateRoute,
+          ),
         ),
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      );
 
-    );
+  /// Handle named route
+  Route _generateRoute(RouteSettings settings) {
+    try {
+      return _doGenerateRoute(settings);
+    } catch (e, s) {
+      debugPrint("failed to generate route for $settings: $e $s");
+      return null;
+    }
   }
+
+  Route _doGenerateRoute(RouteSettings settings) {
+    if (settings.name?.isNotEmpty != true) return null;
+
+    final uri = Uri.parse(settings.name);
+    final path = uri.path ?? '';
+    // final q = uri.queryParameters ?? <String, String>{};
+    switch (path) {
+      case '/note':
+        {
+          final note = (settings.arguments as Map ?? {})['note'];
+          return _buildRoute(settings, (_) => NoteEditor(note: note));
+        }
+      default:
+        return null;
+    }
+  }
+
+  /// Create a [Route].
+  Route _buildRoute(RouteSettings settings, WidgetBuilder builder) =>
+      MaterialPageRoute<void>(
+        settings: settings,
+        builder: builder,
+      );
 }
